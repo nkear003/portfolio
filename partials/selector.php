6ea4f2866e61e07f4eab774b116e58a6cc2a91a8
nkear003 <nkearney757@gmail.com>
2017-07-11 19:58:52 +0200

    changed web design to web development

--- partials/selector.php
+++ partials/selector.php
@@ -2,7 +2,7 @@
   <div class="u-pull-left two columns nav" id="selector">
     <ul>
       <li><a href="work.php"><img src="images/logoBlue.png" class="logo"></a></li>
-      <li><a href="#" id="show_web">Web Design</a></li>
+      <li><a href="#" id="show_web">Web Development</a></li>
       <li><a href="#" id="show_graphic">Graphic Design</a></li>
       <li><a href="#" id="show_all">All</a></li>
       <li><a href="#" class="show_cv">CV</a></li>
