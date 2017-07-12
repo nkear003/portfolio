cf1aa878dd835ac862b4382771b63cf5e537272c
nkear003 <nkearney757@gmail.com>
2017-07-12 18:05:54 +0200

    fixed nesting

--- partials/footer.php
+++ partials/footer.php
@@ -1,10 +1,10 @@
-        </div> <!-- end main-content-->
-    </div> <!--end container-->
-    <footer>
-        <div class="row"><p></p></div>
-    </footer>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
-<script src="js/script.js"></script>
-</body>
+                </div> <!-- end main-content-->
+            </div> <!--end container-->
+            <footer>
+                <div class="row"><p></p></div>
+            </footer>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+        <script src="js/script.js"></script>
+    </body>
 </html>
