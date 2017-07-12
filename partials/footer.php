59dcbe4b0ded6f19c749fbd46ceb7f5398ea2960
nkear003 <nkearney757@gmail.com>
2017-07-12 18:58:21 +0200

    Added 'back to home' and 'back to top'

--- partials/footer.php
+++ partials/footer.php
@@ -1,8 +1,16 @@
                 </div> <!-- end main-content-->
-            </div> <!--end container-->
-            <footer>
-                <div class="row"><p></p></div>
-            </footer>
+            <hr class="no-show">
+            <div class="row">
+                <div class="offset-by-one ten columns" style="text-align:center">
+                    <a href="<?php echo "http://".$_SERVER['SERVER_NAME'].dirname($_SERVER["REQUEST_URI"].'?').'/'; ?>">
+                        Back to Home
+                    </a>
+                     | 
+                    <a href="#" target="_top">Back to Top</a>
+                </div>
+            </div>
+            <hr class="no-show">
+        </div> <!--end container-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
         <script src="js/script.js"></script>
