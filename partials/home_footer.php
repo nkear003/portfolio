35e26e092cc8e9f1aff5c64623866ffc98ae9bb1
nkear003 <nkearney757@gmail.com>
2017-07-12 18:59:16 +0200

    created a home_footer
    the hone page doesn't need a link to home

---
+++ partials/home_footer.php
@@ -0,0 +1,14 @@
+                </div> <!-- end main-content-->
+            <hr class="no-show">
+            <div class="row">
+                <div class="offset-by-one ten columns" style="text-align:center">
+                    <a href="#" target="_top">Back to Top</a>
+                </div>
+            </div>
+            <hr class="no-show">
+        </div> <!--end container-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+        <script src="js/script.js"></script>
+    </body>
+</html>
