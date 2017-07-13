d5a57cf58ef3cb20bf3399c6dcee9f725003891c
nkear003 <nkearney757@gmail.com>
2017-07-13 16:15:36 +0200

    removed jQuery UI script

--- partials/footer.php
+++ partials/footer.php
@@ -8,7 +8,6 @@
             <hr class="no-show">
         </div> <!--end container-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
         <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
         <script src="js/script.js"></script>
     </body>
