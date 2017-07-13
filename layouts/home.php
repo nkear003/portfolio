0acb938d178593543db3eaa5f550148b3082f42d
nkear003 <nkearney757@gmail.com>
2017-07-13 16:18:34 +0200

    cleaning up nesting

--- layouts/home.php
+++ layouts/home.php
@@ -5,8 +5,8 @@
     </head>
     <body>
         <img src="images/logoBlue.png" id="loader_img" class="loader">
-          <div id="main-content">
-          <?php include 'partials/selector.php'; ?>
-          <div class="container">
+            <div id="main-content">
+            <?php include 'partials/selector.php'; ?>
+            <div class="container">
               <?php include 'partials/navbar.php'; ?>
               <?php include 'partials/cv.php'; ?>
