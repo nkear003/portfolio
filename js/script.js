aeab54c023f86e5a85b0675e17440ebb5d93ad12
nkear003 <nkearney757@gmail.com>
2017-08-09 14:56:03 +0200

    Fixed selector

--- js/script.js
+++ js/script.js
@@ -15,18 +15,31 @@ $(document).ready(function() {
     // $('#page-wrap').fadeIn(1000); 
     
     // function to check if web or graphic are active
-    
-    function checkActive () {
+    function notAllActive () {
         
-        // check #show_web to see if active
+        // if web or graphic are active, remove active from all 
         
         if ($('#show_web').hasClass("active") || $('#show_graphic').hasClass("active") ) {
             //remove active class from #show_all    
             $('#show_all').removeClass("active");
             // console.log('#show_web or #show_graphic has active class');
         } else {
+            
+        // if neither web or graphic are active, add active to all
+            
             $('#show_all').addClass("active");
             // console.log('neither #show_web or #show_all have active class');
+        }
+        
+    }
+    
+    // if all has active, remove active from web and graphic
+    function allActive() {    
+        
+        if ($('#show_all').hasClass("active") ) {
+            //remove active from web and graphic
+            $('#show_web').removeClass("active");
+            $('#show_graphic').removeClass("active");
         }
     }
     
@@ -41,8 +54,9 @@ $(document).ready(function() {
         $("#cv").hide("fast");
         
         $(this).toggleClass("active");
+//        $('#show_all').toggleClass("active");
         
-        checkActive();
+        notAllActive();
         
     });
     
@@ -53,8 +67,9 @@ $(document).ready(function() {
         $("#cv").hide("fast");
         
         $(this).toggleClass("active");
+//        $('#show_all').toggleClass("active");
         
-        checkActive();
+        notAllActive();
         
     });
     
@@ -64,6 +79,11 @@ $(document).ready(function() {
         $('.web').show("fast").css("display", "block");
         $('.graphic').show("fast").css("display", "block");
         $("#cv").hide("fast");
+        
+//        allActive();
+        $(this).addClass("active");
+        $('#show_web').removeClass("active");
+        $('#show_graphic').removeClass("active");
     });
     
     // cv
