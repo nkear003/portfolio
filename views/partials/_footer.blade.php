ec985e28df2867845c232e5a2321f68caf1765e9
nkear003 <nkearney757@gmail.com>
2017-08-09 23:39:45 +0200

    changing to 1.4 jquery for hastag change plugin

--- views/partials/_footer.blade.php
+++ views/partials/_footer.blade.php
@@ -1,6 +1,6 @@
 <!--    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>-->
-<!--    <script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js'></script>-->
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!--    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>-->
+    <script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js'></script>
     <script type='text/javascript' src='js/jquery.ba-hashchange.min.js'></script>
     <script type='text/javascript' src='js/dynamicpage.js'></script>
     <script type="text/javascript" src="js/script.js"></script>
