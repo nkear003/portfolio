5e5e655592c568e97c570e2bde3a6ede8a0ac814
nkear003 <nkearney757@gmail.com>
2017-08-10 22:08:50 +0200

    cleaning up _head

--- views/partials/_head.blade.php
+++ views/partials/_head.blade.php
@@ -8,13 +8,8 @@
 <link href="https://fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
-<link rel="stylesheet" href="css/normalize.css">
+<link rel="stylesheet" href="css/normalize.min.css">
 <link rel="stylesheet" href="css/skeleton.min.css">
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/style.min.css">
 
 <link rel="icon" type="image/png" href="images/logoBlueTH.png">
-
-<!--
-<script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js'></script>
-<script type='text/javascript' src='js/jquery.ba-hashchange.min.js'></script>
-<script type='text/javascript' src='js/dynamicpage.js'></script>-->
