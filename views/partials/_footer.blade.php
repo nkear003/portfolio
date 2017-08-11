ff6e8c83267dea8e36978b566feed5da55342e86
nkear003 <nkearney757@gmail.com>
2017-08-11 19:28:31 +0200

    removed onepage scripts

--- views/partials/_footer.blade.php
+++ views/partials/_footer.blade.php
@@ -1,6 +1,6 @@
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script type='text/javascript' src="js/hashtag.min.js"></script>
-    <script type='text/javascript' src='js/dynamicpage.min.js'></script>
+<!--    <script type='text/javascript' src="js/hashtag.min.js"></script>-->
+<!--    <script type='text/javascript' src='js/dynamicpage.min.js'></script>-->
     <script type="text/javascript" src="js/script.min.js"></script>    
     </body>
 </html>
