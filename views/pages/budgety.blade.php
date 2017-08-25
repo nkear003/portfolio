52753fbf74812eb25d26cff120017dc2cfb8ba8a
nkear003 <nkearney757@gmail.com>
2017-08-25 17:09:33 +0200

    changed image

--- views/pages/budgety.blade.php
+++ views/pages/budgety.blade.php
@@ -5,8 +5,10 @@
 @section('content')
 <div class="row">
     <div class="offset-by-one ten columns">
-        <img src="/images/budgety/budgety.png">
-        <hr>
+        <h1 style="margin-top:10px">Budgety</h1>
+        <a href="http://budgety.nicholaskearney.com" target="_blank">
+            <img src="/images/budgety/screenshot.png" alt="Budgety" style="margin-bottom:20px">
+        </a>
         <p>Budgety is a JavaScript budget app, which as I made as a practice project while learning JavaScript.</p>
         <p>You can use the app here:<a href="http://budgety.nicholaskearney.com" target="_blank"><b>http://budgety.nicholaskearney.com</b></a></p>
         <p>You can view the app on GitHub here: <a href="https://github.com/nkear003/budget-app" target="_blank"><b>https://github.com/nkear003/budget-app</b></a></p>
