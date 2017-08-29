eddf7613f6be0eb786b0e018d93342f8aba23d04
nkear003 <nkearney757@gmail.com>
2017-08-29 13:16:39 +0200

    removed facebook and yelp

--- views/pages/phrr.blade.php
+++ views/pages/phrr.blade.php
@@ -19,16 +19,16 @@
         <hr>
     </div>
 </div>
-<div class="row">
+<!--<div class="row">
     <div class="offset-by-one five columns">
         <a href="https://www.facebook.com/poquosonhomerepair/?view_public_for=769685096494961" target="_blank">
-            <img src="images/phrr/phrr_fb.png">
+            <img data-lightbox="" src="images/phrr/phrr_fb.png">
         </a>
     </div>
     <div class="five columns">
         <a href="https://www.yelp.com/biz/poquoson-home-repair-and-remodeling-poquoson" target="_blank">
-            <img src="images/phrr/phrr_yelp.png">
+            <img data-lightbox="" src="images/phrr/phrr_yelp.png">
         </a>
     </div>
-</div>
+</div>-->
 @endsection
