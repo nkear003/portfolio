092abf1bbff5472b755b3608b0132e56028c7f14
nkear003 <nkearney757@gmail.com>
2018-01-12 17:53:47 +0100

    updated navbar links: email & music

--- views/partials/_navbar.blade.php
+++ views/partials/_navbar.blade.php
@@ -1,15 +1,15 @@
 <div class="row">
     <div class="offset-by-one ten columns nav" id="mainnav">
-        <a href="home.php" id="home">Home</a>    
+        <a href="home.php" id="home">Home</a>
         |
         <a href="downloads/Kearney,NicholasCV.pdf" target="_blank">CV</a>
         |
         <a href="https://github.com/nkear003/" target="_blank">GitHub</a>
         |
-        <a href="mailto:nick@nicholaskearney.com">Email</a>
-        |   
+        <a href="mailto:nkearney757@gmail.com">Email</a>
+        |
         <a href="https://www.linkedin.com/in/nicholaskearney" target="_blank">Linkedin</a>
         |
-        <a href="http://nkmusik.com/" target="_blank">Music</a>
+        <a href="http://nicholaskmusic.com/" target="_blank">Music</a>
     </div>
 </div>
